f250ba630a57d7b0e9f30e15fddec28410d48509
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-11-09 22:14:07 +0100

    Add User model

--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -1,7 +1,35 @@
 ﻿namespace RAI_MVC_180348.Models
 {
+    public enum Roles
+    {
+        Default = 0,
+        Admin
+    }
+
     public class UserViewModel
     {
-        public string UserName { get; set; }
+        public string UserName { get; }
+        public Roles Role { get; }
+        public List<string> Friends { get; }
+
+        private string _password;
+
+        public UserViewModel(string userName, string password, Roles role, List<string> friends)
+        {
+            UserName = userName;
+            _password = password;
+            Role = role;
+            Friends = friends;
+        }
+
+        public void AddFriend(string friendName)
+        {
+            Friends.Add(friendName);
+        }
+
+        public void RemoveFriend(string friendName)
+        {
+            Friends.Remove(friendName);
+        }
     }
 }
